0378e42f0ba42e44178f2484f31a08c245fe6102
gracehurley <gehur1@student.monash.edu>
2017-08-11 13:34:58 +1000

    5 poster

--- 5posters/assets/js/main.js
+++ 5posters/assets/js/main.js
@@ -2,9 +2,34 @@ $(document).ready(function() {
   // Add jQuery here
 
   $(".circle").mouseenter(function() {
-    $(".backwards_text.backwards_text2").addClass("showme");
+    $(".backwards_text, .backwards_text2").addClass("showme");
   });
 
   $(".circle").mouseleave(function() {
-    $(".backwards_text.backwards_text2").removeClass("showme");
+    $(".backwards_text, .backwards_text2").removeClass("showme");
+  });
+
+
+
+  var audioBell = document.createElement('audio');
+          audioBell.setAttribute('src', 'assets/images/PushIt.m4a');
+          $.get();
+          audioBell.addEventListener("load", function() {
+            audioBell.play();
+          }, true);
+
+
+    $('.text_wrap_1').click(function() {
+            audioBell.play();
+         });
+
+
+     $('.text_wrap_2').click(function() {
+             audioBell.pause();
+         });
+
+
+
+
+
   });
